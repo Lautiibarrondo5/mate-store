cc1f56b02964a1e4c86aa7e74c790aef108f3643
Lautaro Ibarrondo <ibarrondolautaro379@gmail.com>
2026-06-01 00:52:18 -0300

    Primer commit MateStore

---
+++ js/productos.js
@@ -0,0 +1,124 @@
+//==== BUSCADOR ====
+
+const buscador = document.getElementById("buscador");
+
+buscador.addEventListener("keyup", () => {
+
+    const texto = buscador.value.toLocaleLowerCase();
+
+    const productos = document.querySelectorAll(".producto");
+
+    productos.forEach(producto => {
+
+        const nombre = producto.querySelector("h3")
+        .textContent
+        .toLowerCase();
+
+        if(nombre.includes(texto)){
+            producto.style.display = "block";
+            } else {
+                producto.style.display = "none";
+            }
+    });
+});
+
+// ==== CARRITO ====
+
+const botones = document.querySelectorAll(".btn-carrito");
+const listaCarrito = document.getElementById("lista-carrito");
+const totalHTML = document.getElementById("total");
+const vaciarBtn = document.getElementById("vaciar-carrito");
+const contadorCarrito = document.getElementById("contador-carrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+botones.forEach(boton => {
+
+    boton.addEventListener("click", () => {
+
+        const producto = boton.parentElement;
+
+        const nombre = producto.querySelector("h3").textContent;
+
+        const precio = parseInt(
+            producto.querySelector("p")
+                .textContent
+                .replace("$", "")
+                .replace(/\./g, "")
+        );
+
+        carrito.push({
+            nombre,
+            precio
+        });
+
+        actualizarCarrito();
+
+        alert("Producto agregado al carrito");
+    });
+});
+
+// Eliminar producto
+document.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains("eliminar")) {
+
+        const indice = e.target.dataset.id;
+
+        carrito.splice(indice, 1);
+
+        actualizarCarrito();
+    }
+});
+
+// Vaciar carrito
+if (vaciarBtn) {
+
+    vaciarBtn.addEventListener("click", () => {
+
+        carrito = [];
+
+        actualizarCarrito();
+    });
+}
+
+
+// Actualizar carrito
+function actualizarCarrito() {
+
+    if (listaCarrito) {
+        listaCarrito.innerHTML = "";
+
+        let total = 0;
+
+        carrito.forEach((item, index) => {
+
+            const li = document.createElement("li");
+
+            li.innerHTML = `
+                ${item.nombre}
+                - $${item.precio.toLocaleString("es-AR")}
+                <button class="eliminar" data-id="${index}">
+                    ❌
+                </button>
+            `;
+
+            listaCarrito.appendChild(li);
+
+            total += item.precio;
+        });
+
+        if (totalHTML) {
+            totalHTML.textContent = total.toLocaleString("es-AR");
+        }
+    }
+
+    if (contadorCarrito) {
+        contadorCarrito.textContent = carrito.length;
+    }
+
+    localStorage.setItem(
+        "carrito",
+        JSON.stringify(carrito)
+    );
+}
